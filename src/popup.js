927e40abcc26df0607bdb84be6ce3edf06ea52ba
alejandropintosalcarazo <alejandropintosdev@gmail.com>
2024-07-18 19:17:11 +0200

    queda darle forma al ejercicio

--- src/popup.js
+++
@@ -1,7 +0,0 @@
-// src/popup.js
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import './styles/styles.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
