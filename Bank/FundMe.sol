c5be1c8e5501fda30a6b1084be6c636b42007fe2
alejandropintosalcarazo <alejandropintosdev@gmail.com>
2024-07-21 21:35:15 +0200

    falta añadir modifier y deployear

--- Bank/FundMe.sol
+++ Bank/FundMe.sol
@@ -3,36 +3,45 @@
 pragma solidity ^0.8.18;
 
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./priceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;
 
     uint minUSD = 5e18;
+
+
     address[] public funders;
-    mapping(address => uint256) public addressToAmount; 
+    mapping(address funder => uint256 amountFunded) public addressToAmount; 
+
+    address owner;
+    
+    constructor() {
+        owner = msg.sender;
+    }
 
     function fund() public payable { 
-        require(getConversionRate(msg.value) >= minUSD, "Not enough funds");
-    }
-
-    function getPrice() public view returns(uint) {
-        //address 0x694AA1769357215DE4FAC081bf1f309aDC325306 from https://docs.chain.link/data-feeds/price-feeds/addresses?network=ethereum&page=1
-        //abi chainlink registry contracto to see actual price
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (,int256 price,,,) = priceFeed.latestRoundData();
-       return uint256 (price * 1e10);
-    }
-    function getVersion() public view returns (uint) {
-        return  AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
-    }
-
-    function getConversionRate (uint ethAmount) public view returns (uint) {
-        uint ethPrice = getPrice();
-        uint ethAmountInUSD = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUSD;
+        require(msg.value.getConversionRate() >= minUSD, "Not enough funds");
+        funders.push(msg.sender);
+        addressToAmount[msg.sender] += msg.value;
     }
 
     function Witdthaw () public {
+        require(msg.sender == owner, "You are not the owner");
+        for (uint funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmount[funder] = 0;
+        }
+        funders = new address[](0);
+        ////transfer
+        //payable(msg.sender).transfer(address(this).balance);
+        ////send
+        //bool succesTransfer = payable(msg.sender).send(address(this).balance);
+        //require(succesTransfer, "Transfer failed");
+        ////call
+        (bool callSucces, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSucces, "call failed");
+
     }
 
 }
---
+++ Bank/priceConverter.sol
@@ -0,0 +1,25 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.16;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter {
+        function getPrice() internal view returns(uint) {
+        //address 0x694AA1769357215DE4FAC081bf1f309aDC325306 from https://docs.chain.link/data-feeds/price-feeds/addresses?network=ethereum&page=1
+        //abi chainlink registry contracto to see actual price
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (,int256 price,,,) = priceFeed.latestRoundData();
+       return uint256 (price * 1e10);
+    }
+    function getVersion() internal view returns (uint) {
+        return  AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+    }
+
+    function getConversionRate (uint ethAmount) internal view returns (uint) {
+        uint ethPrice = getPrice();
+        uint ethAmountInUSD = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUSD;
+    }
+
+}
