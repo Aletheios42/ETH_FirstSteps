a3f3ce90ad7804054baf2e94af24ba52355887f4
alejandropintosalcarazo <alejandropintosdev@gmail.com>
2024-08-01 03:35:53 +0200

    importado de chainlink VRF

--- 6.FoundryRaffle/src/Raffle.sol
+++ 6.FoundryRaffle/src/Raffle.sol
@@ -1,23 +1,113 @@
-//SPDX-Lincense-Identifier: MIT
-pragma solidity 0.8.1;
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// internal & private view & pure functions
+// external & public view & pure functions
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 
 /**
  * @title A Simple Raffle contract
- * @author AlejandroPintosAlcarazo
- * @notice This is the last proyect of my initial step in solidity
- * @dev  implement Chainlink VFRv2.5
+ * @author AlejandroPintos
+ * @notice This is the last project of my initial step in Solidity
+ * @dev Implement Chainlink VRF v2.5
  */
+contract Raffle is VRFConsumerBaseV2Plus {
+    /**
+     * ERROR *
+     */
+    error Raffle__SendMoreToEnterRaffle();
 
-contract Raffle {
+    uint256 private immutable i_entranceFee;
+    uint256 private immutable i_interval;
+    address payable[] s_players;
+    uint256 private s_lastTimeStamp;
 
-	uint256 private immutable i_entranceFee;
+    /* State variables */
+    // Chainlink VRF Variables
+    uint256 private immutable i_subscriptionId;
+    bytes32 private immutable i_gasLane;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
 
-	constructor(uint256 entranceFee) {
-		i_entranceFee = entranceFee
-	}
+    event RaffleEntered(address indexed player);
 
-	function enterRaffle() public payable {}
+    /* Functions */
+    constructor(
+        uint256 subscriptionId,
+        bytes32 gasLane, // keyHash
+        uint256 interval,
+        uint256 entranceFee,
+        uint32 callbackGasLimit,
+        address vrfCoordinatorV2
+    ) VRFConsumerBaseV2Plus(vrfCoordinatorV2) {
+        i_gasLane = gasLane;
+        i_interval = interval;
+        i_subscriptionId = subscriptionId;
+        i_entranceFee = entranceFee;
+        s_lastTimeStamp = block.timestamp;
+        i_callbackGasLimit = callbackGasLimit;
+        // uint256 balance = address(this).balance;
+        // if (balance > 0) {
+        //     payable(msg.sender).transfer(balance);
+        // }
+    }
 
-	function pickWinner() public {}
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle__SendMoreToEnterRaffle();
+        }
+        s_players.push(payable(msg.sender));
+        emit RaffleEntered(msg.sender);
+    }
 
+    function pickWinner() public {
+        if (block.timestamp - s_lastTimeStamp > i_interval) {
+            revert();
+        }
+        // Will revert if subscription is not set and funded.
+        uint256 requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: i_gasLane,
+                subId: i_subscriptionId,
+                requestConfirmations: REQUEST_CONFIRMATIONS,
+                callbackGasLimit: i_callbackGasLimit,
+                numWords: NUM_WORDS,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+                )
+            })
+        );
+    }
+
+    function fulfillRandomWords(uint256 requestIdm, uint256[] calldata randomWords) internal virtual override {}
+    /**
+     * GETTERS *
+     */
+
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
 }
