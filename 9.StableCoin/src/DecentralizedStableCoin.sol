29aff947eaa1e4f89e549fae86a9360750efb982
Aletheios42 <alejandropintosdev@gmail.com>
2024-08-18 03:33:18 +0200

    Contrato del la stablecoin hehco falta todo el ngine

---
+++ 9.StableCoin/src/DecentralizedStableCoin.sol
@@ -0,0 +1,67 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// internal & private view & pure functions
+// external & public view & pure functions
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+import {ERC20, ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title DecentralizedStableCoin
+ * @author Aletheios42
+ * Colateral: Exogenous (ETH & BTC)
+ * Minting: Algoritmic
+ * Relative Stability: Pegged to $
+ *
+ * this contract is the ERC20 implementation meant to be  govern by DCEngine
+ */
+contract DecentralizedStableCoin is ERC20, ERC20Burnable, Ownable {
+    error DecentralizedStableCoin__MustBeMoreThanZero();
+    error DecentralizedStableCoin__BurnAmountExceedsBalance();
+    error DecentralizedStableCoin__NotZeroAddress();
+
+    // error DecentralizedStableCoin__
+
+    constructor() ERC("DecentralizedStableCoin", "DSC") {}
+
+    function burn(uint256 _amount) public override {
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount < 0) {
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
+        }
+        super.burn(_amount);
+    }
+
+    function mint(
+        address _to,
+        uint256 _amount
+    ) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert DecentralizedStableCoin__NotZeroAddress();
+        }
+        if (_amount < 0) {
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
+        }
+        _mint(_to, _amount);
+        return true
+    }
+}
