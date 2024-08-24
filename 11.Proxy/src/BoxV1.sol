6759a6c2b55d2f8d7586e301e0fefabf97becc92
Aletheios42 <alejandropintosdev@gmail.com>
2024-08-24 23:10:25 +0200

    Boxterminados

--- 11.Proxy/src/BoxV1.sol
+++ 11.Proxy/src/BoxV1.sol
@@ -1,8 +1,22 @@
 // SPDX-License-Identifier:MIT
 pragma solidity ^0.8.24;
 
-contract BoxV1 {
+import {UPPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UPPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract BoxV1 is Initializable, UPPSUpgradeable, OwnableUpgradeable {
     uint256 internal number;
+
+    ///@custom_oz-upgrades-unsafe-allow contructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() public initializer {
+        __Ownable_init(); //calls tranferOwnnership with msg.sender
+        __UUPSUpgradeable_init();
+    }
 
     function getNumber() external view returns (uint256) {
         return number;
@@ -11,4 +25,8 @@ contract BoxV1 {
     function version() external pure returns (uint256) {
         return 1;
     }
+
+    function _authorizeUpgrade(
+        address newImplementation
+    ) internal override onlyOwner {}
 }
--- 11.Proxy/src/BoxV2.sol
+++ 11.Proxy/src/BoxV2.sol
@@ -1,10 +1,26 @@
 // SPDX-License-Identifier:MIT
 pragma solidity ^0.8.24;
 
-contract BoxV2 {
+import {UPPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UPPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract BoxV2 Initializable, UPPSUpgradeable, OwnableUpgradeable {
     uint256 internal number;
 
-    function setNumber(uint256 _number) external {}
+    ///@custom_oz-upgrades-unsafe-allow contructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() public initializer {
+        __Ownable_init(); //calls tranferOwnnership with msg.sender
+        __UUPSUpgradeable_init();
+    }
+
+    function setNumber(uint256 _number) external {
+      number = _number;
+    }
 
     function getNumber() external view returns (uint256) {
         return number;
@@ -13,4 +29,7 @@ contract BoxV2 {
     function version() external pure returns (uint256) {
         return 2;
     }
+    function _authorizeUpgrade(
+        address newImplementation
+    ) internal override onlyOwner {}
 }
