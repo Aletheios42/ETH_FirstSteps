9e64e19b5163c953d9694e5e223fba2916a32ad8
Aletheios42 <alejandropintosdev@gmail.com>
2024-08-24 05:41:58 +0200

    terminado

--- 10.MerkleAirdrop/script/Interact.s.sol
+++ 10.MerkleAirdrop/script/Interact.s.sol
@@ -26,14 +26,37 @@ contract ClaimAirdrop is Script {
         hex"fbd2270e6f23fb5fe9248480c0f4be8a4e9bd77c3ad0b1333cc60b5debc511602a2a06c24085d8d7c038bad84edc53664c8ce0346caeaa3570afec0e61144dc11c";
 
     function claimAirdrop(address airdrop) public {
-        MerkleAirdrop(airdrop).claim();
+        vm.startBroadcast();
+        (uint8 v, bytes32 r, bytes32 s) = splitSignature(SIGNATURE);
+        MerkleAirdrop(airdrop).claim(
+            CLAIMING_ADDRESS,
+            AMOUNT_TO_COLLECT,
+            proof,
+            v,
+            r,
+            s
+        );
+        vm.stopBroadcast();
     }
 
-    function run() external view {
+    function run() external {
         address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
             "MerkleAirdrop",
             block.chainid
         );
         claimAirdrop(mostRecentlyDeployed);
+    }
+
+    function splitSignature(
+        bytes memory sig
+    ) public pure returns (uint8 v, bytes32 r, bytes32 s) {
+        if (sig.length != 65) {
+            revert __ClaimAirdropScript__InvalidSignatureLength();
+        }
+        assembly {
+            r := mload(add(sig, 32))
+            s := mload(add(sig, 64))
+            v := byte(0, mload(add(sig, 96)))
+        }
     }
 }
